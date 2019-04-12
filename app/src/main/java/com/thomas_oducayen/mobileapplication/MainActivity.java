b3c6358c7d76908861bd22e0eb3de28904eb46de
Thomas <tcoducayen@gmail.com>
2019-04-12 11:29:49 -0700

    added listener to top left button

--- app/src/main/java/com/thomas_oducayen/mobileapplication/MainActivity.java
+++ app/src/main/java/com/thomas_oducayen/mobileapplication/MainActivity.java
@@ -2,12 +2,27 @@ package com.thomas_oducayen.mobileapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.tlButton);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Button is clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
