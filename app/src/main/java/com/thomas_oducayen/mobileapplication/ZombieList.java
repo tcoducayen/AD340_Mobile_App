4fdf09b5ef7b6c3d21e89b74d14ebe48e36fbf45
Thomas <tcoducayen@gmail.com>
2019-04-24 00:24:55 -0700

    added year to view

--- app/src/main/java/com/thomas_oducayen/mobileapplication/ZombieList.java
+++ app/src/main/java/com/thomas_oducayen/mobileapplication/ZombieList.java
@@ -4,21 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ZombieList extends AppCompatActivity {
     ListView listView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zombie_list);
-        listView = findViewById(R.id.mobile_list);
-        String[] zombieTitles = getTitles();
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.zombie_list_view, R.id.textView, zombieTitles);
-        listView.setAdapter(arrayAdapter);
-    }
-
     public String[][] zombies = {
             {"Night of the Comet","1984","","http://collider.com/wp-content/uploads/2016/10/night-of-comet.jpg","What would kids in the 1980s do if the apocalypse blew through the world without them noticing? Hang out at the mall, but of course. That’s the set-up for this very funny, quite dated horror-comedy, which begins when a quartet of adolescents lock themselves inside a projection booth at the mall’s multiplex. This somehow allows them to live through an extinction level event of some sort, which has also left roaming bands of murderous mutants. Catherine Mary Stewart of the equally inexplicable Weekend at Bernie’s leads the film, but it’s a movie of mood more than substance ultimately. Does the wealth-fueled naiveté of the average white teenager survive in a vacuum? Does it go away when they are being hunted for sustenance? It’s an interesting to watch on these terms and when the zombies show up, director Thom Eberhardt adds menace and a tight feel for suspense to the action sequences. And if we’re being honest, it belongs on this list for its soundtrack alone. The rest of this is just whip cream and cherries. – Chris Cabin"},
             {"Dead Snow","2009","Tommy Wirkola","http://collider.com/wp-content/uploads/2016/07/dead-snow.jpg","With so many zombie movies over the years, eventually you’re going to run out of ways to freshen up the sub-genre. Enter Wirkola’s decidedly skewed take on zombies in this horror-comedy with plenty of guts. Sure, zombies are great movie monsters, but if you have Nazi zombies, well you’ve just doubled-down on the level of villainy (and pun-worthiness) in your picture! \nThis splatter-fest puts a Nordic spin on the traditional zombie by adding in elements of the Draugr, an undead creature from Scandinavian folklore that fiercely protects its treasure horde. In the case of Dead Snow, these draugr happen to be former SS soldiers who terrorized a Norwegian town and looted their belongings, only to be done in or chased into the freezing mountains by the villagers themselves. Dead Snow gets originality points for this, for sure. It’s also a very funny, gory, and satisfyingly violent movie with elements of Evil Dead and “teen sex/slasher” flicks scattered throughout. And if you like it, there’s more where that came from in the sequel, Dead Snow: Red vs Dead. – Dave Trumbore"},
@@ -42,6 +35,30 @@ public class ZombieList extends AppCompatActivity {
             {"Shawn of the Dead","2004","Edgar Wright","http://collider.com/wp-content/uploads/2014/10/shaun-of-the-dead-image.jpg","When it comes to advancing the zombie movie genre, this film stands out among the crowd. It’s both a warm-blooded love letter to the history of zombie films and a stone-cold laugher that takes shots at those very same films. It helped quite a bit that Wright and Pegg enjoyed a shorthand from working together on the TV series Is It Bill Bailey? and Spaced before collaborating on what is arguably the best zombie comedy out there today.\nShaun of the Dead didn’t just take the zombie movie tropes and run with them, it allowed Wright’s meticulous attention to detail and brand of humor, complemented by Pegg’s delivery, to bubble to the surface. It’s one of the most re-watchable movies out there because you’ll always find something new that you may have missed dozens of times before. And once you’ve steeped yourself in the history of zombie films, you should also find that you have a new appreciation for just how lovingly Shaun of the Dead was put together. – Dave Trumbore"},
             {"Dawn of the Dead","1978","George Romero","http://collider.com/wp-content/uploads/2015/08/dawn-of-the-dead-romero.jpg","George Romero didn’t invent the zombie, but he did single-handedly create the template for the modern zombie movie as we know it with his exquisite Night of the Living Dead. For his sequel, Romero dodged the temptation to retread familiar territory (a quality he would maintain for each of his subsequent “dead” films), ditching the intimate confines of a home for the sprawling reaches of a shopping mall, and trading his black-and-white bleakness for a playful color-saturated palette.\nDawn of the Dead is a horror sequel in every sense, bigger and bloodier, but it maintain’s Romero’s commitment to piercing social commentary, this time tackling the insatiable lust of American consumerism. It’s also packed to the brim with Romero’s skilled eye for visceral violence rendered with first-rate old-school gore effects from Tom Savini, the legendary craftsman of carnage who transplanted his experience as a combat photographer in Vietnam to a career spent creating on-screen nightmares. As in all of Romero’s great work, that beautifully executed bloodshed is only a backdrop for a compelling character drama as the group of strangers seeking refuge in the abandoned shopping complex cope with increasing interpersonal conflict. Romero directs it all with wit and empathy, and an expert eye for when to drop the next big scare. — Haleigh Foutch"}
     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zombie_list);
+        listView = findViewById(R.id.mobile_list);
+
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("lvTitle", zombies[i][0]);
+            hm.put("lvYear", zombies[i][1]);
+            hm.put("lvImage", zombies[i][3]);
+            aList.add(hm);
+        }
+
+        String[] from = { "lvImage", "lvTitle", "lvYear" };
+        int[] to = { R.id.listview_image, R.id.listview_title, R.id.listview_year};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.zombie_list_view, from, to);
+        ListView androidListView =  findViewById(R.id.mobile_list);
+        androidListView.setAdapter(simpleAdapter);
+    }
 
     public String[] getTitles() {
         String[] zombieTitles = new String[zombies.length];
