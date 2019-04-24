d63102a5da5a380507b7ae7ef3c2bc345242c277
Thomas <tcoducayen@gmail.com>
2019-04-23 22:17:52 -0700

    updated main activity to correct view functions

--- app/src/main/java/com/thomas_oducayen/mobileapplication/MainActivity.java
+++ app/src/main/java/com/thomas_oducayen/mobileapplication/MainActivity.java
@@ -5,57 +5,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button tl = (Button) findViewById(R.id.tlButton);
+        Button tl = findViewById(R.id.tlButton);
         tl.setOnClickListener(this);
-        Button tr = (Button) findViewById(R.id.trButton);
+        Button tr = findViewById(R.id.trButton);
         tr.setOnClickListener(this);
-        Button bl = (Button) findViewById(R.id.blButton);
+        Button bl = findViewById(R.id.blButton);
         bl.setOnClickListener(this);
-        Button br = (Button) findViewById(R.id.brButton);
+        Button br = findViewById(R.id.brButton);
         br.setOnClickListener(this);
-        Button send = (Button) findViewById(R.id.send_button);
+        Button send = findViewById(R.id.send_button);
         send.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
-
             case R.id.tlButton:
-                Toast.makeText(getApplicationContext(),
-                        "Top left button was clicked", Toast.LENGTH_LONG).show();
+                startZombies();
                 break;
-
             case R.id.trButton:
-                Toast.makeText(getApplicationContext(),
-                        "Top right button was clicked", Toast.LENGTH_LONG).show();
+                displayToast("Top right");
                 break;
-
             case R.id.blButton:
-                Toast.makeText(getApplicationContext(),
-                        "Bottom left button was clicked", Toast.LENGTH_LONG).show();
+                displayToast("Bottom left");
                 break;
-
             case R.id.brButton:
-                Toast.makeText(getApplicationContext(),
-                        "Bottom right button was clicked", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.send_button:
-                Toast.makeText(getApplicationContext(),
-                        "Message sent! (Not really, just mock up)", Toast.LENGTH_LONG).show();
+                displayToast("Bottom right");
                 break;
             default:
                 break;
         }
     }
+
+    public void startZombies() {
+        Intent intent = new Intent(this, ZombieList.class);
+        startActivity(intent);
+    }
+
+    public void displayToast(String direction) {
+        Toast.makeText(getApplicationContext(),
+                direction + " button was clicked", Toast.LENGTH_LONG).show();
+    }
+
 }
